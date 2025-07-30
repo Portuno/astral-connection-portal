95a10773daa4edf2938ab3502b295336df9c025a
Portuno <lautaro.sarni@gmail.com>
2025-07-30 13:25:52 +0200

    chat

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const OnboardingWrapper = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
-      <MetaPixelProvider pixelId={import.meta.env.VITE_META_PIXEL_ID || '151594632270912121221212344'}>
+      <MetaPixelProvider pixelId={import.meta.env.VITE_META_PIXEL_ID || '1515946322777777'}>
         <TooltipProvider>
           <Toaster />
           <BrowserRouter>
